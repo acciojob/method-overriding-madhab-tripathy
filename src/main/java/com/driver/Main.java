0aea09bfcf6c33a21691950e83545fcb8ee5853e
Madhab Tripathy <madhabtripathy1349@gmail.com>
2023-02-08 00:03:46 +0530

    Class file deleted

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,3 +6,16 @@ public class Main {
         System.out.println(obj1.meth());
     }
 }
+class A {
+    public String meth() {
+        return "Invoking method from class A";
+    }
+}
+
+// Class B
+class B extends A {
+    @Override
+    public String meth() {
+        return "Method is overridden in Extendend class B";
+    }
+}
