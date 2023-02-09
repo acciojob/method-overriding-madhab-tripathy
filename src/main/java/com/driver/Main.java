6dbb852930afc25fa3d2ddb389283bc08c06db29
Madhab Tripathy <madhabtripathy1349@gmail.com>
2023-02-09 16:41:24 +0530

    Overriden Successfull

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,21 +1,25 @@
 package com.driver;
 
 public class Main {
+    public static class A {
+        String meth() {
+            return "Invoking method from class A";
+        }
+    }
+
+    // Class B
+    public static class B extends A {
+        @Override
+        String meth() {
+            return "Method is overridden in Extended class B";
+        }
+    }
     public static void main(String[] args) {
         B obj1 = new B();
         System.out.println(obj1.meth());
-    }
-}
-class A {
-    public String meth() {
-        return "Invoking method from class A";
+        B obj2 = new B();
+        System.out.println(obj2.meth());
     }
 }
 
-// Class B
-class B extends A {
-    @Override
-    public String meth() {
-        return "Method is overridden in Extendend class B";
-    }
-}
+
